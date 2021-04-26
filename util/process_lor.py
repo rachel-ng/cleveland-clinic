967205b0539f2e59c57dfb6b9b5c5b2ce1bd413e
rachel <rng@stuy.edu>
2021-04-26 16:27:30 -0400

    process lor

--- util/process_lor.py
+++ util/process_lor.py
@@ -1,7 +1,8 @@
 import os
+import glob 
 
 from datetime import datetime
-from ocr_attr_utils import image_conversion, extract_text
+from util import image_conversion, extract_text
 
 def get_find_box_args(processed_lor):
     """
@@ -17,15 +18,33 @@ def get_find_box_args(processed_lor):
     return page_src_args
 
 
-def process_lor_to_boxes(pdf_path, png_dest_path='./drive/MyDrive/DFG/dev_data', det=False):
+def get_lor_applicant(pdf_path):
+    '''Extract text from LOR PDF and return dictionary with info from first line'''
+    if (os.path.isfile(pdf_path)):
+        extracted_text = extract_text.get_pdf_text(pdf_path)
+
+        # get applicant info
+        line = [i.strip() for i in extracted_text[0].split("\n", 1)[0].replace(")", "(").split("(")]
+        applicant = {"name": line[0],
+                     "id": line[1],
+                     "author": line[2].split("-")[-1].strip()
+                     }
+
+        return applicant
+    else:
+        print("no such file at {}".format(pdf_path))
+        return None
+
+
+def process_lor_to_boxes(pdf_path, png_dest_path='/content/drive/MyDrive/DFG/LOR_Data/dev_data', det=False):
     '''Return processed '''
+
     elapsed = datetime.now()
     if (os.path.isfile(pdf_path)):
         # attempt to extract text from pdf
         if det: print("get extracted text from {}".format(pdf_path))
         extracted_text = extract_text.get_pdf_text(pdf_path)
 
-        start = datetime.now()
         # get applicant info
         line = [i.strip() for i in extracted_text[0].split("\n", 1)[0].replace(")", "(").split("(")]
         applicant = {"name": line[0],
@@ -34,18 +53,18 @@ def process_lor_to_boxes(pdf_path, png_dest_path='./drive/MyDrive/DFG/dev_data',
                      }
 
         if det: print(line, "\n", applicant)
-        print("applicant info: {}s".format(datetime.now() - start))
 
         # convert pdf to png, if pngs already exist at dest they won't be converted
         if det: print("convert pdf to pngs")
         ocr_text = []
         png_template, png_pages = image_conversion.convert_pdf_png(pdf_path, applicant['id'], png_dest_path)
 
-        if det: print("ocr")
         # ocr
+        if det: print("ocr")
         for page_path in png_pages:
-            ocr_text.append(extract_text.process_ocr(page_path, det=det))
-            # print(ocr_text[-1])
+            ocr_text.append(extract_text.process_ocr(page_path))
+            #print(ocr_text[-1])
+        if det: print("ocr done")
 
         # ocr_text = the list of text information per detected textbox.
 
@@ -56,9 +75,12 @@ def process_lor_to_boxes(pdf_path, png_dest_path='./drive/MyDrive/DFG/dev_data',
                          'ocr text': [[j['text'] for j in i] for i in ocr_text],
                          'ocr text full': ocr_text
                          }
+
         print("elapsed: {}s\n".format(datetime.now() - elapsed))
+
         return processed_lor
 
     else:
         print("no such file at {}".format(pdf_path))
         return None
+
